23d44fa1476da6af81cc9a86a20925370b927d5f
volcanoeyes <130888597+volcanoeyes@users.noreply.github.com>
2024-02-03 00:44:04 +0800

    add CSRF protection

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/justinas/nosurf"
 	"snippetbox.volcanoeyes.net/internal/models"
 )
 
@@ -16,6 +17,7 @@ type templateData struct {
 	Form            any
 	Flash           string
 	IsAuthenticated bool
+	CSRFToken       string
 }
 
 func humanDate(t time.Time) string {
@@ -64,5 +66,6 @@ func (app *application) newTemplateData(r *http.Request) *templateData {
 		CurrentYear:     time.Now().Year(),
 		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
 		IsAuthenticated: app.isAuthenticated(r),
+		CSRFToken:       nosurf.Token(r),
 	}
 }
